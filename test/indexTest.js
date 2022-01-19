88a1bfc92d8ec88f26ca7707d90723c8c872f475
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-01-19 21:46:50 +0000

    wip

---
+++ test/indexTest.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect 
+const should = require('chai').should()    // notice we use '()' here. 
+const assert = require('chai').assert 
+
+describe("testing function xxxx", () => {
+
+	it("testing correct number of paramers passed in", () => {
+		let result = true
+
+		expect(result).to.be.true
+
+		result.should.be.true
+
+		assert.isTrue(result)
+
+	})	
+
+})
